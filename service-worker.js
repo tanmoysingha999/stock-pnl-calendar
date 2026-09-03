c0c1a8ba30a745f3fa51c520d14d90a1b8c96a05
tanmoysingha999 <tanmoy.singha100@gmail.com>
2026-09-03 13:28:24 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='stock-pnl-calendar-v48-final-3points';
+const CACHE='stock-pnl-calendar-v49-final-3points';
 const CORE=['./index.html?v=46'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
