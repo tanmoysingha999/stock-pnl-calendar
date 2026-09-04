915fa35773a04e05ecacbb6d7c32418ed112affa
tanmoysingha999 <tanmoy.singha100@gmail.com>
2026-09-05 01:12:26 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='stock-pnl-calendar-v52-partial-sell-pnl-fix-final';
+const CACHE='stock-pnl-calendar-v52-main-page-calc-display-fix-final';
 const SHELL='./index.html';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim();})());});
